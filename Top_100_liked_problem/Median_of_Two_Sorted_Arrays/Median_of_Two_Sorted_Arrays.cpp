ddae262a61a15ef8fe983de35491f83e60923845
fieryheart <fiery_heart@163.com>
2019-04-09 18:21:02 +0800

    2019-04-09-update-Problem-1&4

--- Top_100_liked_problem/Median_of_Two_Sorted_Arrays/Median_of_Two_Sorted_Arrays.cpp
+++ Top_100_liked_problem/Median_of_Two_Sorted_Arrays/Median_of_Two_Sorted_Arrays.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+    // 使用sort排序解决问题
+    double findMedianSortedArrays1(vector<int>& nums1, vector<int>& nums2) {
         // if(nums1.size() == 0) {
         //     int len2 = nums2.size();
         //     return (nums2[(len2-1)/2] + nums2[len2/2]) / 2.0;
@@ -21,15 +22,50 @@ public:
         int len = nums1.size();
         return (nums1[(len-1)/2] + nums1[len/2]) / 2.0;
     }
+    // O(log(m+n))解法
+    // 参考链接：https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2481/Share-my-O(log(min(mn))-solution-with-explanation
+    double findMedianSortedArrays2(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> vm, vn;
+        if(nums1.size() < nums2.size()) {
+            vm = nums1;
+            vn = nums2;
+        } else {
+            vm = nums2;
+            vn = nums1;
+        }
+        int m = vm.size(), n = vn.size();
+        int imin = 0, imax = m;
+        while(1) {
+            int i = (imin + imax) / 2, j = (m + n + 1)/2 - i;
+            if(i > 0 && vm[i-1] > vn[j]) {
+                imax = i - 1;
+            } else if(i < m && vn[j-1] > vm[i]) {
+                imin = i + 1;
+            } else {
+                int max_of_left, min_of_right;
+                if(i == 0) max_of_left = vn[j-1];
+                else if(j == 0) max_of_left = vm[i-1];
+                else max_of_left = max(vm[i-1], vn[j-1]);
+
+                if((m+n)%2) return max_of_left / 1.0;
+
+                if(i == m) min_of_right = vn[j];
+                else if(j == n) min_of_right = vm[i];
+                else min_of_right = min(vm[i], vn[j]);
+
+                return (max_of_left + min_of_right) / 2.0;
+            }
+        }
+    }
 };
 
 
 int main()
 {
-    vector<int> nums1 = {1,3};
-    vector<int> nums2 = {2};
+    vector<int> nums1 = {1,2};
+    vector<int> nums2 = {3,4};
     Solution s;
-    double rst = s.findMedianSortedArrays(nums1, nums2);
+    double rst = s.findMedianSortedArrays2(nums1, nums2);
     cout << rst << endl;
     return 0;
 }
--- Top_100_liked_problem/Two_Sum/Two_Sum.cpp
+++ Top_100_liked_problem/Two_Sum/Two_Sum.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        bool flag = false;
+    vector<int> twoSum1(vector<int>& nums, int target) {
         vector<int> rst;
         for(int i = 0; i < nums.size(); ++i)
         {
@@ -13,7 +13,6 @@ public:
             {
                 if(nums[i] + nums[j] == target)
                 {
-                    flag = true;
                     rst.push_back(i);
                     rst.push_back(j);
                     break;
@@ -22,17 +21,30 @@ public:
         }
         return rst;
     }
+    // hash解法
+    vector<int> twoSum2(vector<int>& nums, int target) {
+        vector<int> rst;
+        map<int, int> mapi;
+        map<int, bool> mapb;
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            if(mapb[target-nums[i]]) {
+                rst.push_back(mapi[target-nums[i]]);
+                rst.push_back(i);
+                break;
+            }
+            mapb[nums[i]] = true;
+            mapi[nums[i]] = i;
+        }
+        return rst;
+    }    
 };
 int main()
 {
     vector<int> nums = {2, 7, 11, 15};
-    // nums.push_back(2);
-    // nums.push_back(7);
-    // nums.push_back(11);
-    // nums.push_back(15);
     int target = 9;
     Solution s;
-    vector<int> rst = s.twoSum(nums, target);
+    vector<int> rst = s.twoSum2(nums, target);
     if(rst.size()) {
         cout << "[" << rst[0] << ", " << rst[1] << "]" << endl;
     } else {
